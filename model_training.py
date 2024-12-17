42770d11380ef773fe925989e00b6d4fc687f31f
Jared Zirkes <jaredzrks0@gmail.com>
2024-12-17 14:01:52 -0500

    Added Colbert commands

---
+++ model_training.py
@@ -0,0 +1,8 @@
+# Code adapted from the COLBERT Github Readme
+
+## NOTE: BEFORE RUNNING, ENSURE THAT ALL COLBERT DATA AND CHECKPOINTS ARE SUCCESFULLY INSTALLED
+## TO DO THIS RUN THE FOLLOWING COMMANDS:
+## !git -C ColBERT/ pull || git clone https://github.com/stanford-futuredata/ColBERT.git > /dev/null 2>&1
+##!wget "https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz"
+##!mkdir -p checkpoints  # Create the 'checkpoints' directory if it doesn't exist
+##!tar -xvzf colbertv2.0.tar.gz -C checkpoints
